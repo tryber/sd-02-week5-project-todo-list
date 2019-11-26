90383459e48a11e6828642935183ce41a21829c2
ishidor <kevinmendessilva@hotmail.com>
2019-11-26 17:53:18 -0300

    Checking and Unchecking can be done

--- Kevin/script.js
+++ Kevin/script.js
@@ -1,38 +1,58 @@
-window.onload = function (){
-    let toDoItems = []
+window.onload = function () {
+let todoItems = [];
 
-    function addToDo (text) {
-        const toDo ={
-            text,
-            checked: false,
-            id: Date.now(),
-        };
-        toDoItems.push(toDo);
-        
-        const list = document.querySelector(".js-todo-list");
-        
-        list.insertAdjacentHTML ("beforeend", `
-        <li class="todo-item" data-key="${toDo.id}">
-            <input id="${toDo.id}" type="checkbox" />
-            <label for="${toDo.id}" class="tick js-tick"></label>
-            <span>${toDo.text}</span>
-            <button class="delete-todo js-delete-todo">
-                <svg><use href="#delete-icon"></use></svg>
-            </button>
-        </li>
-        `);
-    }
-    const form = document.querySelector('.js-form');
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        const input = document.querySelector('.js-todo-input');
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
 
-        const text = input.value.trim();
+  todoItems.push(todo);
 
-        if (text !== "") {
-            addToDo(text);
-            input.value = "";
-            input.focus();
-        }
-    })
+  const list = document.querySelector('.js-todo-list');
+  list.insertAdjacentHTML('beforeend', `
+    <li class="todo-item" data-key="${todo.id}">
+      <input id="${todo.id}" type="checkbox"/>
+      <label for="${todo.id}" class="tick js-tick"></label>
+      <span>${todo.text}</span>
+      <button class="delete-todo js-delete-todo">
+        <svg><use href="#delete-icon"></use></svg>
+      </button>
+    </li>
+  `);
+}
+
+function toggleDone(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+
+  const item = document.querySelector(`[data-key='${key}']`);
+  if (todoItems[index].checked) {
+    item.classList.add('done');
+  } else {
+    item.classList.remove('done');
+  }
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('.js-todo-input');
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+});
 }
