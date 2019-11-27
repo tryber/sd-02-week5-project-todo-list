7f97c48cf6e0a6673123ff0787c14bb6dbe2bc3a
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2019-11-27 19:46:54 -0200

    Code Climate correcoes

--- JulioCezar/script.js
+++ JulioCezar/script.js
@@ -1,47 +1,51 @@
-window.onload = function () {
-  const buttonAdd = document.getElementById('button_tarefas');
-  buttonAdd.addEventListener("click", adicionar);
-  const buttonClear = document.getElementById('button_apagar_lista');
-  buttonClear.addEventListener("click", apagarLista);
-  const tarefasLi = document.getElementById('ol_content');
-  tarefasLi.addEventListener("dblclick", tarefaCompleta);
-  const buttonTarefasCompletas = document.getElementById('button_remover_completos');
-  buttonTarefasCompletas.addEventListener("click", removerTarefasCompletas);
-}
+/*global window .*/
+/*global document .*/
 
-function adicionar (event) {
-  let text = document.getElementById('input_tarefas').value;
-  let addTarefas = document.createElement('li');
-  addTarefas.classList.add('lista_tarefas');  
-  let textNode = document.createTextNode(text);
+function adicionar() {
+  const text = document.getElementById('input_tarefas').value;
+  const addTarefas = document.createElement('li');
+  addTarefas.classList.add('lista_tarefas');
+  const textNode = document.createTextNode(text);
   addTarefas.appendChild(textNode);
   document.getElementById('ol_lista_tarefas').appendChild(addTarefas);
-  document.getElementById('input_tarefas').value = '';   
-}
+  document.getElementById('input_tarefas').value = '';
+};
 
-function tarefaCompleta (event) {
-  let listaTexto = event.target;
+function tarefaCompleta(event) {
+  const listaTexto = event.target;
   if (listaTexto.style.textDecoration === 'line-through') {
     listaTexto.style.textDecoration = 'none';
   } else {
     listaTexto.style.textDecoration = 'line-through';
   }
-}
+};
 
-function removerTarefasCompletas (event) {
+function removerTarefasCompletas() {
   const listaTarefas = document.getElementById('ol_lista_tarefas');
-  let tamanhoLista = document.querySelectorAll('#ol_lista_tarefas li').length;
-  for (i=tamanhoLista-1;i>=0;i--){
-    let listaTexto = listaTarefas.childNodes[i].style.textDecoration;
-    if(listaTexto === 'line-through')
-    listaTarefas.removeChild(listaTarefas.childNodes[i]);
+  const tamanhoLista = document.querySelectorAll('#ol_lista_tarefas li').length;
+  for (let i = tamanhoLista-1; i >= 0; i --) {
+    const listaTexto = listaTarefas.childNodes[i].style.textDecoration;
+    if (listaTexto === 'line-through') {
+      listaTarefas.removeChild(listaTarefas.childNodes[i]);   
+    }
   }
-}
+};
 
-function apagarLista (event) {
+function apagarLista() {
   const listaTarefas = document.getElementById('ol_lista_tarefas');
-  let tamanhoLista = document.querySelectorAll('#ol_lista_tarefas li').length;
-  for (i=0;i<tamanhoLista;i++){
+  const tamanhoLista = document.querySelectorAll('#ol_lista_tarefas li').length;
+  for (let i = 0; i < tamanhoLista; i ++) {
     listaTarefas.removeChild(listaTarefas.childNodes[0]);
   }
-}
+};
+
+window.onload = function start() {
+  const buttonAdd = document.getElementById('button_tarefas');
+  buttonAdd.addEventListener('click', adicionar);
+  const buttonClear = document.getElementById('button_apagar_lista');
+  buttonClear.addEventListener('click', apagarLista);
+  const tarefasLi = document.getElementById('ol_content');
+  tarefasLi.addEventListener('dblclick', tarefaCompleta);
+  const buttonTarefasCompletas = document.getElementById('button_remover_completos');
+  buttonTarefasCompletas.addEventListener('click', removerTarefasCompletas);
+};
