f93c173fcc6753474685e6bd6af615356dc4a268
phtf92 <phtf92@gmail.com>
2019-11-28 17:18:01 -0300

    Adiocinei riscar

--- pedro-tofani/script.js
+++ pedro-tofani/script.js
@@ -1,21 +1,28 @@
 
 let ponteiroItemLista = document.getElementsByClassName('itemLista');
 
-function selecionarItemLista (){
-    if (this.style.color == 'red') {this.style.color = 'black'}
-    else {this.style.color = 'red'};
-}
+
 
 let ponteiroBotaoAdicionarTarefa = document.getElementById('adicionarTarefa');
 let ponteiroConteudoDoInput = document.getElementById('descricaoTarefa');
 let ponteiroOL = document.getElementById('listaDeAfazeres');
 ponteiroBotaoAdicionarTarefa.addEventListener('click', adicionarTarefa);
 
-
-function adicionarTarefa(){
+function adicionarTarefa() {
     let criarLi = document.createElement('li')
     criarLi.innerHTML = ponteiroConteudoDoInput.value;
     criarLi.className = 'itemLista'
     criarLi.addEventListener('click', selecionarItemLista)
+    criarLi.addEventListener('dblclick', riscar)
     ponteiroOL.appendChild(criarLi);
+}
+
+function selecionarItemLista() {
+    if (this.style.color == 'red') { this.style.color = 'black' }
+    else { this.style.color = 'red' };
+}
+
+function riscar() {
+    if (this.style.textDecoration == 'line-through') this.style.textDecoration = '';
+    else this.style.textDecoration = 'line-through'
 }
