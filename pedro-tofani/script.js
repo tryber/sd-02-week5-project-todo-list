2f2b72d18390632bdf5015e2687b208d07dd77e9
phtf92 <phtf92@gmail.com>
2019-11-28 17:08:47 -0300

    Botao de adicionar item à lista

--- pedro-tofani/script.js
+++ pedro-tofani/script.js
@@ -1,11 +1,21 @@
 
 let ponteiroItemLista = document.getElementsByClassName('itemLista');
 
-for (let i = 0; i < ponteiroItemLista.length; i++){
-    ponteiroItemLista[i].addEventListener('click', selecionarItemLista)
-}
-
 function selecionarItemLista (){
     if (this.style.color == 'red') {this.style.color = 'black'}
     else {this.style.color = 'red'};
+}
+
+let ponteiroBotaoAdicionarTarefa = document.getElementById('adicionarTarefa');
+let ponteiroConteudoDoInput = document.getElementById('descricaoTarefa');
+let ponteiroOL = document.getElementById('listaDeAfazeres');
+ponteiroBotaoAdicionarTarefa.addEventListener('click', adicionarTarefa);
+
+
+function adicionarTarefa(){
+    let criarLi = document.createElement('li')
+    criarLi.innerHTML = ponteiroConteudoDoInput.value;
+    criarLi.className = 'itemLista'
+    criarLi.addEventListener('click', selecionarItemLista)
+    ponteiroOL.appendChild(criarLi);
 }
