6e3aaecaf1a69920e957a0083e364afcdd0f7ca0
Douglas Henrique <doughrqe@gmail.com>
2019-11-28 22:26:58 -0300

    Quase finalizando requisitos obrigatorios

--- Douglas/script.js
+++ Douglas/script.js
@@ -1,17 +1,70 @@
 const ok = document.getElementById ('envia');
 let entrada = document.getElementById ('entrada');
-const pegaOl= document.querySelector('ol');
+let pegaOl = document.querySelector ('ol');
+const botoes = document.querySelectorAll ('button' );
+let contador=3;
+const apagaTudo = document.getElementById ('apagarTudo')
+const apagaRealizado = document.getElementById ('apagarCompletos');
+const item= document.querySelectorAll ('item');
+window.onload = function () {
+this.click ();
+this.trocaMouse ();
+this.removeTudo ();
+};
 
-ok.addEventListener ( 'click', function () {
+function removeRealizado(){
+  apagaRealizado.addEventListener('click', function(){
+
+  })
+}
+
+function removeTudo(){
+  apagaTudo.addEventListener('click', function () {
+  pegaOl.innerHTML='';
+});
+}
+
+function click () {
+  ok.addEventListener ( 'click', function () {
   adicionaLinha (entrada.value);
-})
+});
+};
 
 function adicionaLinha (argumento) {
   const criaLi = document.createElement ('li');
   pegaOl.appendChild (criaLi);
   criaLi.innerText = argumento;
   criaLi.classList = ('class', 'item');
-}
+  entrada.value = '';
+  risca(criaLi);
+  marca(criaLi);
+};
 
-const itens = document.querySelectorAll('item');
-itens.addEventListener('click')
+function risca (criaLi) {
+  let j =0;
+  criaLi.addEventListener ('dblclick', function () {
+    criaLi.classList.toggle("mystyle");
+})
+};
+
+function marca (criaLi) {
+  let i=0;
+  criaLi.addEventListener ('click', function () {
+    if ( i == 0 ){
+      criaLi.style.backgroundColor='blue';
+      i = 1;
+    }
+    else {
+      criaLi.style.backgroundColor='white';
+      i=0;
+    }
+  });
+};
+
+function trocaMouse () {
+  for( let i = 0 ; i < botoes.length; i++ ){
+    botoes[i].addEventListener ('mouseover', function () {
+    botoes[i].style.cursor = "pointer";
+    });
+  };
+};
