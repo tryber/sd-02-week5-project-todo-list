cfc85c64f6320684c549443216431eaf6e09f44b
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-29 17:01:57 -0300

    progress

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,10 +1,30 @@
 let newTask= document.getElementById('newTask')
-
+let clear = document.getElementById('clear')
 let addTask = document.getElementById('addTask')
-    addTask.addEventListener('click', function(){
+
+addTask.addEventListener('click', function(){
     if (newTask.value == '') {
         alert("Sem tarefas por hoje?");
         } else {
+        fAddTask()
+    }
+})
+
+clear.addEventListener('click', function(){
+    let lista = document.getElementById('list')
+    while (lista.firstChild){
+    lista.removeChild(lista.firstChild)
+    }
+})
+
+newTask.addEventListener('keypress', function(e) {
+    if(e.keyCode == 13 && newTask.value == '') {
+        alert ("Sem tarefas por hoje?")
+    } else if (e.keyCode == 13) {
+    fAddTask() }
+})
+
+function fAddTask () {
     let newItem = document.createElement('li');
     newItem.innerText = newTask.value;
     newItem.className = 'item';
@@ -12,22 +32,12 @@ let addTask = document.getElementById('addTask')
     document.getElementById('newTask').value ='';
     newTask.value = null;
     newItem.addEventListener('click', function() {
-    event.target.style.backgroundColor = 'red';
-    event.target.style.color = 'blue'; 
+    event.target.style.color = 'yellow'; 
     }
     )       
-
-
-    }})
-
-    let clear = document.getElementById('clear')
-    clear.addEventListener('click', function(){
-        let lista = document.getElementById('list')
-        while (lista.firstChild){
-        lista.removeChild(lista.firstChild)
-        }
-    })
+ }
+    
+   
 
     // let done = getElementById('done')
     // done.
-
