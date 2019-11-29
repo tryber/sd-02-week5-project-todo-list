c552b91257f0eee02d46309a7941fc6b32bfbb2b
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-29 14:51:11 -0300

    add button clear and fixed input bugs

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,7 +1,39 @@
-let textInput = document.getElementById('newTask')
-    textInput.addEventListener('change', function(){
-    let texto = this.value
-    let lista = document.getElementById('lista')
-    let novoItem = document.createElement('li')
+let newTask= document.getElementById('newTask')
+    newTask.addEventListener('change', function(){
+    newTask = this.value;}
+    );
+    
+let addTask = document.getElementById('addTask')
+    addTask.addEventListener('click', function(){
+    if (newTask.value == '' || newTask == '') {
+        alert("Sem tarefas por hoje?");
+        } else {
+    let newItem = document.createElement('li');
+    newItem.innerText = newTask;
+    newItem.className = 'item';
+    document.getElementById('list').appendChild(newItem);
+    document.getElementById('newTask').value ='';
+    newTask = '';
+    newItem = '';
 
+    }})
+
+    // let highlight = document.getElementsByClassName('item')
+    // highlight.addEventListener('click', function () {
+    
+    // }
+    // )
+
+    //limpar
+
+    let clear = document.getElementById('clear')
+    clear.addEventListener('click', function(){
+        let lista = document.getElementById('list')
+        while (lista.firstChild){
+        lista.removeChild(lista.firstChild)
+        }
+    })
+
+    // let done = getElementById('done')
+    // done.
 
