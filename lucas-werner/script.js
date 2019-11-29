879ae21e810b475585706153df824d80dfe57006
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-29 16:31:24 -0300

    funções ajeitadas

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,30 +1,24 @@
 let newTask= document.getElementById('newTask')
-    newTask.addEventListener('change', function(){
-    newTask = this.value;}
-    );
-    
+
 let addTask = document.getElementById('addTask')
     addTask.addEventListener('click', function(){
-    if (newTask.value == '' || newTask == '') {
+    if (newTask.value == '') {
         alert("Sem tarefas por hoje?");
         } else {
     let newItem = document.createElement('li');
-    newItem.innerText = newTask;
+    newItem.innerText = newTask.value;
     newItem.className = 'item';
     document.getElementById('list').appendChild(newItem);
     document.getElementById('newTask').value ='';
-    newTask = '';
-    newItem = '';
+    newTask.value = null;
+    newItem.addEventListener('click', function() {
+    event.target.style.backgroundColor = 'red';
+    event.target.style.color = 'blue'; 
+    }
+    )       
+
 
     }})
-
-    // let highlight = document.getElementsByClassName('item')
-    // highlight.addEventListener('click', function () {
-    
-    // }
-    // )
-
-    //limpar
 
     let clear = document.getElementById('clear')
     clear.addEventListener('click', function(){
