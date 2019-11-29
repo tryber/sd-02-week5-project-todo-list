c2aa6d519ff7f8a9c47e305e7ff487628c77a918
fernandoluckesi <fernando.luckesi94@gmail.com>
2019-11-28 21:57:18 -0300

    class adicionada aos <li> do ol com id = listaOrdenada

--- projetos/lista-de-tarefas/script.js
+++ projetos/lista-de-tarefas/script.js
@@ -4,6 +4,7 @@ let listaOrdenada = document.getElementById('lista');
 let item;
 
 adicionar.addEventListener('click', function(){
+    
     if(inputItem.value == ""){
         alert('Espaço em branco. Digite algo e clique em Adicionar');
     }else{
@@ -18,12 +19,18 @@ listaOrdenada.addEventListener('click', function(event){
     event.target.style.backgroundColor = '#4169E1';
 })
 
+let completadas;
+
 listaOrdenada.addEventListener('dblclick', function(event){
     if(event.target.style.textDecoration == 'line-through'){
-        event.target.style.textDecoration = 'none'
+        event.target.style.textDecoration = 'none';
+        event.target.setAttribute('class', '');
     }else{
         event.target.style.textDecoration = 'line-through'
+        event.target.setAttribute('class', 'completada');
+        completadas = document.getElementsByClassName('completada');
     }
+    console.log(completadas.length);
 })
 
 let botaoLimparCompletos = document.getElementById('limpar-completos');
@@ -31,18 +38,20 @@ let botaoLimparCompletos = document.getElementById('limpar-completos');
 let tarefas = listaOrdenada.children;
 
 botaoLimparCompletos.addEventListener('click', function(){
-    for(let i = 0; i < tarefas.length; i++){
-        if(tarefas[i].style.textDecoration == 'line-through'){
-            listaOrdenada.removeChild(tarefas[i])
-        } 
-    } 
+    
+     for(let i = 0; i < completadas.length; i++){
+        completadas[i].remove();
+    }
 })
 
 let botaoLimparLista = document.getElementById('limpar-lista');
 
 botaoLimparLista.addEventListener('click', function(){
-    for(let i = 0; i < tarefas.length; i++){
+    listaOrdenada.innerHTML = "";
+    /* for(let i = 0; i < tarefas.length; i++){
         console.log(tarefas[i].innerText)
-        /* listaOrdenada.removeChild(tarefas[i]); */
-    }
+         listaOrdenada.removeChild(tarefas[i]); 
+    } */
 })
+
+
