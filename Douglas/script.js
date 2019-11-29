b6a9ff2f9085e90cb6e7c8cbc2ff0b2270916d75
Douglas Henrique <doughrqe@gmail.com>
2019-11-29 17:42:03 -0300

    code climate

--- Douglas/script.js
+++ Douglas/script.js
@@ -1,74 +1,69 @@
-const ok = document.getElementById ('envia');
-let entrada = document.getElementById ('entrada');
-let pegaOl = document.querySelector ('ol');
-const botoes = document.querySelectorAll ('button' );
-const apagaTudo = document.getElementById ('apagarTudo')
-const apagaRealizado = document.getElementById ('apagarCompletos');
-const item= document.querySelectorAll ('item');
-let apaga = document.querySelectorAll ('.risca');
+const ok = document.getElementById('envia');
+const entrada = document.getElementById('entrada');
+const pegaOl = document.querySelector('ol');
+const botoes = document.querySelectorAll('button');
+const apagaTudo = document.getElementById('apagarTudo');
+const apagaRealizado = document.getElementById('apagarCompletos');
+let apaga = document.querySelectorAll('.risca');
 
-window.onload = function () {
-  this.click ();
-  this.trocaMouse ();
-  this.removeTudo ();
-  this.apagaRiscado ();
+window.onload = function inicia () {
+  click();
+  trocaMouse();
+  // removeTudo(pegaOl);
+  apagaRiscado();
 };
 
-function removeTudo () {
-  apagaTudo.addEventListener ('click', function () {
-  pegaOl.innerHTML='';
-});
+// function removeTudo(pegaOl) {
+//     apagaTudo.addEventListener('click', function () {
+//     pegaOl.innerHTM = "";
+//   });
+// }
+
+function risca(criaLi) {
+  criaLi.addEventListener('dblclick', function riscando() {
+    criaLi.classList.toggle('risca');
+    apaga = document.querySelectorAll('.risca');
+  });
 }
 
-function click () {
-  ok.addEventListener ( 'click', function () {
-  adicionaLinha (entrada.value);
-});
-};
+function marca(criaLi) {
+  criaLi.addEventListener('click', function marcando() {
+    const unico = document.querySelectorAll('destaca');
+    criaLi.classList.toggle("destaca");
+  });
+}
 
-function adicionaLinha (argumento) {
-  const criaLi = document.createElement ('li');
-  pegaOl.appendChild (criaLi);
+function adicionaLinha(argumento) {
+  const criaLi = document.createElement('li');
+  pegaOl.appendChild(criaLi);
   criaLi.innerText = argumento;
   criaLi.classList = ('class', 'item');
   entrada.value = '';
-  risca (criaLi);
-  marca (criaLi);
-};
+  risca(criaLi);
+  marca(criaLi);
+}
 
-function risca (criaLi) {
-  let j =0;
-  criaLi.addEventListener ('dblclick', function () {
-    criaLi.classList.toggle ("risca");
-    apaga = document.querySelectorAll ('.risca');
-})
-};
-
-function marca (criaLi) {
-  criaLi.addEventListener ('click', function () {
-    let unico= document.querySelectorAll('destaca');
-    for(let i = 0; i < unico.lenght; i++ ){
-      unico[i].classList.remove('destaca');
-    }
-    criaLi.classList.add ("destaca");
+function click() {
+  ok.addEventListener('click', function incluiLinha() {
+    adicionaLinha(entrada.value);
   });
-};
+}
 
-function trocaMouse () {
-  for( let i = 0 ; i < botoes.length; i++ ){
-    botoes[i].addEventListener ('mouseover', function () {
-    botoes[i].style.cursor = "pointer";
-    botoes[i].style.backgroundColor = 'cornsilk';
-    });
-    botoes[i].addEventListener ('mouseout', function () {
+function trocaMouse() {
+  for (let i = 0; i < botoes.length; i += 1) {
+      botoes[i].addEventListener('mouseover', function passsarMouse() {
+      botoes[i].style.cursor = 'pointer';
+      botoes[i].style.backgroundColor = 'cornsilk';
+      });
+      botoes[i].addEventListener('mouseout', function mouseMao() {
       botoes[i].style.backgroundColor = 'ButtonFace';
-    });
-  };
-};
+      });
+  }
+}
 
-function apagaRiscado (){
-  apagaRealizado.addEventListener('click', function (){
-    for( let i=0; i< apaga.length ; i++){
+function apagaRiscado() {
+  apagaRealizado.addEventListener('click', function apgaRiscado() {
+    for (let i = 0; i< apaga.length; i += 1) {
       pegaOl.removeChild(apaga[i]);
     }
   });
