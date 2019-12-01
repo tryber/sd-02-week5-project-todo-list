fd3d3184bf2e54def26654b66f08189af64b2060
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-12-01 19:49:59 -0300

    Bug Squash Anterior: 19 faltando

--- johnatas-henrique/js/script.js
+++ johnatas-henrique/js/script.js
@@ -4,40 +4,41 @@ const listaOrdenada = document.querySelector('ol');
 const botaoApagarRiscadas = document.querySelector('.apagar-riscadas');
 const botaoApagarSelecionada = document.querySelector('.apagar-selecionada');
 const botaoApagarTudo = document.querySelector('.apagar-tudo');
+let buscaClasse;
 
 /* Selecionar tarefa - single-click */
 
 function selecionaTarefa(event) {
-    const tamLista = listaOrdenada.querySelectorAll('li').length;
-    for (let i = 0; i < tamLista; i += 1) {
-      listaOrdenada.querySelectorAll('li')[i].classList.remove('tarefa-selecionada');
-    }
-    const evento = event;
-    evento.target.classList += ' tarefa-selecionada';
+  const tamLista = listaOrdenada.querySelectorAll('li').length;
+  for (let i = 0; i < tamLista; i += 1) {
+    listaOrdenada.querySelectorAll('li')[i].classList.remove('tarefa-selecionada');
   }
-  
-  function singleClickTarefa(item) {
-    item.addEventListener('click', selecionaTarefa);
-  }
+  const evento = event;
+  evento.target.classList += ' tarefa-selecionada';
+}
 
-  /* Riscar tarefa double-click */
+function singleClickTarefa(item) {
+  item.addEventListener('click', selecionaTarefa);
+}
+
+/* Riscar tarefa double-click */
 
 function riscaTarefa(event) {
-    const listaTodasClasses = event.target.className;
-    const buscaRisco = listaTodasClasses.indexOf('tarefa-riscada');
-    const evento = event;
-    if (buscaRisco !== -1) {
-      evento.target.classList.remove('tarefa-riscada');
-      evento.target.classList += ' tarefa-ativa';
-    } else {
-      evento.target.classList.remove('tarefa-ativa');
-      evento.target.classList += ' tarefa-riscada';
-    }
+  const listaTodasClasses = event.target.className;
+  const buscaRisco = listaTodasClasses.indexOf('tarefa-riscada');
+  const evento = event;
+  if (buscaRisco !== -1) {
+    evento.target.classList.remove('tarefa-riscada');
+    evento.target.classList += ' tarefa-ativa';
+  } else {
+    evento.target.classList.remove('tarefa-ativa');
+    evento.target.classList += ' tarefa-riscada';
   }
-  
-  function doubleClickTarefa(item) {
-    item.addEventListener('dblclick', riscaTarefa);
-  }
+}
+
+function doubleClickTarefa(item) {
+  item.addEventListener('dblclick', riscaTarefa);
+}
 
 /* Escrever texto do input na li */
 
@@ -54,40 +55,40 @@ function textonaLista() {
 
 criarTarefa.addEventListener('click', textonaLista);
 
-/* Botão apagar riscados */
-
-function apagarRiscadas() {
-  const itemListaOrdenada = listaOrdenada.querySelectorAll('li');
-  for (let i = 0; i < itemListaOrdenada.length; i += 1) {
-    const listaTodasClasses = itemListaOrdenada[i].className;
-    const buscaRisco = listaTodasClasses.indexOf('tarefa-riscada');
-    if (buscaRisco !== -1) {
-      itemListaOrdenada[i].parentNode.removeChild(itemListaOrdenada[i]);
-    }
-  }
-}
-
-botaoApagarRiscadas.addEventListener('click', apagarRiscadas);
-
 /* Botão apagar tudo */
 
 function apagarTudo() {
-  listaOrdenada.innerHTML = '';
-}
+    listaOrdenada.innerHTML = '';
+  }
+  
+  botaoApagarTudo.addEventListener('click', apagarTudo);  
 
-botaoApagarTudo.addEventListener('click', apagarTudo);
+/* Botão apagar riscados */
 
-/* Botão apagar selecionado */
-
-function apagarSelecionada() {
-    const itemListaOrdenada = listaOrdenada.querySelectorAll('li');
+function apagarItemsLista() {
+  const itemListaOrdenada = listaOrdenada.querySelectorAll('li');
   for (let i = 0; i < itemListaOrdenada.length; i += 1) {
     const listaTodasClasses = itemListaOrdenada[i].className;
-    const buscaRisco = listaTodasClasses.indexOf('tarefa-selecionada');
+    const buscaRisco = listaTodasClasses.indexOf(buscaClasse);
     if (buscaRisco !== -1) {
       itemListaOrdenada[i].parentNode.removeChild(itemListaOrdenada[i]);
     }
   }
 }
 
-botaoApagarSelecionada.addEventListener('click', apagarSelecionada);
+function buscaRiscadas(){
+    buscaClasse = 'tarefa-riscada';
+    apagarItemsLista();
+}
+
+botaoApagarRiscadas.addEventListener('click', buscaRiscadas);
+
+/* Botão apagar selecionado */
+
+function buscaSelecionada(){
+    buscaClasse = 'tarefa-selecionada';
+    apagarItemsLista();
+}
+
+
+botaoApagarSelecionada.addEventListener('click', buscaSelecionada);
