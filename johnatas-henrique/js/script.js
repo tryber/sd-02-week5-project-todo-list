a1315b141b2f1e5529bb6e020349df47951a3c9c
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-12-01 19:35:04 -0300

    Bug Squash Anterior: 18 faltando

--- johnatas-henrique/js/script.js
+++ johnatas-henrique/js/script.js
@@ -5,6 +5,40 @@ const botaoApagarRiscadas = document.querySelector('.apagar-riscadas');
 const botaoApagarSelecionada = document.querySelector('.apagar-selecionada');
 const botaoApagarTudo = document.querySelector('.apagar-tudo');
 
+/* Selecionar tarefa - single-click */
+
+function selecionaTarefa(event) {
+    const tamLista = listaOrdenada.querySelectorAll('li').length;
+    for (let i = 0; i < tamLista; i += 1) {
+      listaOrdenada.querySelectorAll('li')[i].classList.remove('tarefa-selecionada');
+    }
+    const evento = event;
+    evento.target.classList += ' tarefa-selecionada';
+  }
+  
+  function singleClickTarefa(item) {
+    item.addEventListener('click', selecionaTarefa);
+  }
+
+  /* Riscar tarefa double-click */
+
+function riscaTarefa(event) {
+    const listaTodasClasses = event.target.className;
+    const buscaRisco = listaTodasClasses.indexOf('tarefa-riscada');
+    const evento = event;
+    if (buscaRisco !== -1) {
+      evento.target.classList.remove('tarefa-riscada');
+      evento.target.classList += ' tarefa-ativa';
+    } else {
+      evento.target.classList.remove('tarefa-ativa');
+      evento.target.classList += ' tarefa-riscada';
+    }
+  }
+  
+  function doubleClickTarefa(item) {
+    item.addEventListener('dblclick', riscaTarefa);
+  }
+
 /* Escrever texto do input na li */
 
 function textonaLista() {
@@ -20,48 +54,10 @@ function textonaLista() {
 
 criarTarefa.addEventListener('click', textonaLista);
 
-/* Selecionar tarefa - single-click */
-
-function selecionaTarefa(event) {
-  const tamLista = listaOrdenada.querySelectorAll('li').length;
-  for (let i = 0; i < tamLista; i += 1) {
-    listaOrdenada.querySelectorAll('li')[i].classList.remove('tarefa-selecionada');
-  }
-  evento = event
-  evento.target.classList += ' tarefa-selecionada';
-}
-
-function singleClickTarefa(item) {
-  item.addEventListener('click', selecionaTarefa);
-}
-
-listaOrdenada.querySelectorAll('li').forEach(singleClickTarefa);
-
-/* Riscar tarefa double-click */
-
-function riscaTarefa(event) {
-  const listaTodasClasses = event.target.className;
-  const buscaRisco = listaTodasClasses.indexOf('tarefa-riscada');
-  const evento = event
-  if (buscaRisco !== -1) {
-    evento.target.classList.remove('tarefa-riscada');
-    evento.target.classList += ' tarefa-ativa';
-  } else {
-    evento.target.classList.remove('tarefa-ativa');
-    evento.target.classList += ' tarefa-riscada';
-  }
-}
-
-function doubleClickTarefa(item) {
-  item.addEventListener('dblclick', riscaTarefa);
-}
-
-listaOrdenada.querySelectorAll('li').forEach(doubleClickTarefa);
-
 /* Botão apagar riscados */
 
 function apagarRiscadas() {
-  itemListaOrdenada = listaOrdenada.querySelectorAll('li');
+  const itemListaOrdenada = listaOrdenada.querySelectorAll('li');
   for (let i = 0; i < itemListaOrdenada.length; i += 1) {
     const listaTodasClasses = itemListaOrdenada[i].className;
     const buscaRisco = listaTodasClasses.indexOf('tarefa-riscada');
@@ -84,7 +80,7 @@ botaoApagarTudo.addEventListener('click', apagarTudo);
 /* Botão apagar selecionado */
 
 function apagarSelecionada() {
-  itemListaOrdenada = listaOrdenada.querySelectorAll('li');
+    const itemListaOrdenada = listaOrdenada.querySelectorAll('li');
   for (let i = 0; i < itemListaOrdenada.length; i += 1) {
     const listaTodasClasses = itemListaOrdenada[i].className;
     const buscaRisco = listaTodasClasses.indexOf('tarefa-selecionada');
