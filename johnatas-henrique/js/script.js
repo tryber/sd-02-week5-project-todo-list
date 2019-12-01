9427ef8152009acbe7913cc512a0a6423f6a021f
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-12-01 15:16:37 -0300

    JS - Riscar Tarefa

--- johnatas-henrique/js/script.js
+++ johnatas-henrique/js/script.js
@@ -1,19 +1,53 @@
 const criarTarefa = document.querySelector('.criar-tarefa');
 const textoTarefa = document.querySelector('#texto-tarefa');
-const listaOrdenada = document.querySelector('ol');
+listaOrdenada = document.querySelector('ol');
 
 /* Escrever texto do input na li */
 
-function textonaLista (){
-    let itemLista = document.createElement('li');
-    itemLista.innerText = textoTarefa.value;
-    itemLista.className = 'tarefa tarefa-ativa';
-    textoTarefa.value = '';
-    listaOrdenada.appendChild(itemLista);
+function textonaLista() {
+  let itemLista = document.createElement('li');
+  itemLista.innerText = textoTarefa.value;
+  itemLista.className = 'tarefa tarefa-ativa';
+  textoTarefa.value = '';
+  listaOrdenada.appendChild(itemLista);
+  listaOrdenada.querySelectorAll('li').forEach(singleClickTarefa);
+  listaOrdenada.querySelectorAll('li').forEach(doubleClickTarefa);
 }
 
 criarTarefa.addEventListener('click', textonaLista);
 
+/* Selecionar tarefa - single-click */
 
+function selecionaTarefa(event) {
+  tamLista = listaOrdenada.querySelectorAll('li').length;
+  for (let i = 0; i < tamLista; i++) {
+    listaOrdenada.querySelectorAll('li')[i].classList.remove('tarefa-selecionada');
+  }
+  event.target.classList += ' tarefa-selecionada';
+}
 
+function singleClickTarefa(item) {
+  item.addEventListener('click', selecionaTarefa);
+}
 
+listaOrdenada.querySelectorAll('li').forEach(singleClickTarefa);
+
+/* Riscar tarefa double-click */
+
+function riscaTarefa(event) {
+  listaTodasClasses = event.target.className;
+  buscaRisco = listaTodasClasses.indexOf('tarefa-riscada')
+  if (buscaRisco != -1) {
+    event.target.classList.remove('tarefa-riscada');
+    event.target.classList += ' tarefa-ativa';
+  } else {
+    event.target.classList.remove('tarefa-ativa');
+    event.target.classList += ' tarefa-riscada';
+  }
+}
+
+function doubleClickTarefa(item) {
+  item.addEventListener('dblclick', riscaTarefa);
+}
+
+listaOrdenada.querySelectorAll('li').forEach(doubleClickTarefa);
