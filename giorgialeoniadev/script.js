349f96340cba9f61913b3131783947d810c5bb70
Giorgia <giorgialeoniac@gmail.com>
2019-12-01 22:02:32 -0300

    Fazendo com que o input fique vazio após a inserção da tarefa

--- giorgialeoniadev/script.js
+++ giorgialeoniadev/script.js
@@ -14,6 +14,7 @@ function criaTarefa() {
         const novaTarefa = document.createElement("li");
         novaTarefa.append(entrada);
         document.getElementsByTagName("ol")[0].append(novaTarefa);
+        input.value = "";
     }
 }
 
