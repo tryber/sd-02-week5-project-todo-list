192df3684cf9fda745de346ada426cd33affa1bd
EddyeBoy27 <lipe0503@gmail.com>
2019-12-02 04:26:05 -0200

    Atualizando Commit com atualizaçoes no JavaScript

--- Felipe-Lista/script.js
+++ Felipe-Lista/script.js
@@ -1,21 +1,37 @@
+// Adiciona Item na Lista
+
 let textoFormulario = document.querySelector('#textoFormulario')
-const listaAtual = document.querySelectorAll('.listaTarefas')[0]
-const botaoEnviar = document.querySelector('#botaoEnviar')
-const elementoAtual = document.querySelectorAll('ol')
+let botaoEnviar = document.querySelector('#botaoEnviar')
+let elementoAtual = document.querySelectorAll('ol')
 let conteudoElemento;
+let lista
 
-
-
+let listaAtual = document.querySelectorAll('.listaTarefas')
+let listaIndividual;
 
 function itemListaPreenchido(event){
     event.preventDefault()
     novoElemento = document.createElement('li')
+    novoElemento.className = "itemLista"
     novoElemento.innerText = textoFormulario.value
     elementoAtual[0].appendChild(novoElemento)
     textoFormulario.value = "";
+
 }
 
+elementoAtual.forEach(elemento => {
+    elemento.onclick = () => {
+        let click = elementoAtual[0].getElementsByClassName('itemLista')
+        console.log(click)
+    } 
+  })
+
+function adicionaClasse (event){
+
+}
 
 botaoEnviar.addEventListener ( 'click', itemListaPreenchido )
+
+// Seleciona Item da Lista
 
 
