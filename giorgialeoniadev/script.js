0b11c7c26eb420009793e91e2da467a17c4b2c42
Giorgia <giorgialeoniac@gmail.com>
2019-12-01 21:55:20 -0300

    Alteração no head do projeto eliminando aspas no title, alterações na forma de criar as tarefas e mudança no JS, alterações no CSS.

--- giorgialeoniadev/script.js
+++ giorgialeoniadev/script.js
@@ -1,14 +1,20 @@
-
-//Funcão que insere a tarefa
-function addTarefa() {
-    let tarefa = document.getElementById("input");
-    let novaTarefa = do
-        let novaTarefa = document.createElement("li");
-document.getElementsByTagName("ol")[0].appendChild(novaTarefa);
+function iniciar() {
+    const botao = document.querySelector(".botaoAdicionar");
+    botao.addEventListener('click', function () {
+        criaTarefa();
+    })
 }
 
-//Ação de clicar no botão
-let button = document.getElementsByClassName("botaoAdicionar")[0];
-button.addEventListener("click", function () {
-    addTarefa()
-})
+function criaTarefa() {
+    const input = document.querySelector("#input");
+    const entrada = input.value;
+    if (entrada.trim() === ""){
+        alert("Por favor, digite um texto para sua tarefa.");
+    } else {
+        const novaTarefa = document.createElement("li");
+        novaTarefa.append(entrada);
+        document.getElementsByTagName("ol")[0].append(novaTarefa);
+    }
+}
+
+iniciar();
