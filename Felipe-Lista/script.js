4bbb97ff04c71f6ba2798d5a0b3c0ebe7e1eec67
EddyeBoy27 <lipe0503@gmail.com>
2019-12-03 10:49:01 -0200

    Finalizado ToDo list

--- Felipe-Lista/script.js
+++ Felipe-Lista/script.js
@@ -1,43 +1,79 @@
 // Adiciona Item na Lista
-let corSelecionado = 'transparent'
 
 let textoFormulario = document.querySelector('#textoFormulario');
-let botaoEnviar = document.querySelector('#botaoEnviar');
-const elementoAtual = document.querySelector('ol');
-let itemSelecionado=document.querySelectorAll('li');
+const botaoEnviar = document.querySelector('#botaoEnviar');
+let elementoAtual = document.querySelector('ol');
 
-const trocarCor = (elemento, cor) => {
-    elemento.style.backgroundColor = cor
+function itemListaPreenchido() {
+  let novoElemento = document.createElement('li');
+  novoElemento.className = "itemLista";
+  novoElemento.innerText = textoFormulario.value;
+  elementoAtual.appendChild(novoElemento);
+  textoFormulario.value = '';
+  textoFormulario.focus();
+
+}
+
+botaoEnviar.addEventListener('click', itemListaPreenchido);
+
+
+//Apaga item selecionado
+
+let apagarTudo = document.querySelector('#apagarTudo')
+
+apagarTudo.onclick = () => {
+  limpaTudo = document.getElementsByClassName('itemLista')
+  limpaTudo.length
+  if (limpaTudo.length >= 1) {
+    for (i = 0; i = limpaTudo.length; i++) {
+      limpaTudo[length].remove(i)
+    }
+  } else {
+    alert("Não há itens para excluir")
+  }
+  limpaTudo.length
+}
+
+//Seleciona item
+
+elementoAtual.addEventListener('click', function () {
+  resultado = event.target
+  
+  if (resultado.className == "itemLista"){
+    resultado.className = "itemLista selecionado"
+  } else {
+    resultado.className = "itemLista"
   }
 
-const selecionaItem = (event) => {
-    selecionado = event.target
-    selecionado.className = "selecionado"
-}
+})
 
-const itemDaListaClique = (event) => {
-    selecionado = event.target
-    selecionado.className = "itemLista selecionado"
-}
+//Apaga Selecionado
 
+let apagarSelecionado = document.querySelector('#apagarSelecionado')
 
-function itemListaPreenchido(){
-    let novoElemento = document.createElement('li');
-    novoElemento.className = "itemLista";
-    novoElemento.onclick = itemDaListaClique
-    novoElemento.innerText = textoFormulario.value;
-    elementoAtual.appendChild(novoElemento);
-    textoFormulario.value = '';
-    textoFormulario.focus();
-
-}
-
-botaoEnviar.addEventListener ( 'click', itemListaPreenchido );
-
-
-itemSelecionado.forEach(element => {
-    element.onclick = () => {
-        selecionaItem
+apagarSelecionado.onclick = () => {
+  limpaSelecionado = document.getElementsByClassName('selecionado')
+  limpaSelecionado.length
+  if (limpaSelecionado.length >= 1) {
+    for (i = 0; i = limpaSelecionado.length; i++) {
+      limpaSelecionado[length].remove(i)
     }
-});
+  } else {
+    alert("Não há itens selecionados para apagar")
+  }
+}
 
+
+//Double Click Risca Item
+
+elementoAtual.addEventListener('dblclick', function () {
+    resultado = event.target
+    console.log(resultado.classList.contains('itemLista'))
+    if (resultado.classList.contains('riscado')){
+        resultado.classList.remove("riscado")
+      } else {
+        resultado.classList.add('riscado')
+      }
+    
+  
+  })
