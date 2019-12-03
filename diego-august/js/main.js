8006631c55cb2c3e69761041722ef2244446cf09
Diego Augusto S F <contactdiegonow@gmail.com>
2019-12-03 01:15:38 -0300

    mudança geral inicial, alteração de CSS e identidade visual ao site

--- diego-august/js/main.js
+++ diego-august/js/main.js
@@ -1,138 +1,55 @@
-var taskInput=document.getElementById("new-task");//adicionar novas tarefas
-var addButton=document.getElementsByTagName("button")[0]
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul de tarefas incompletas
-var completedTasksHolder=document.getElementById("completed-tasks");//tarefas completas
-
-
-//Novo item
-var createNewTaskElement=function(taskString){
-
-	var listItem=document.createElement("li");
-
-	//input (checkbox)
-	var checkBox=document.createElement("input");//checkbx
-	//label
-	var label=document.createElement("label");//label
-	//input (text)
-	var editInput=document.createElement("input");//text
-	//button.edit
-	var editButton=document.createElement("button");//edit button
-
-	//button.delete
-	var deleteButton=document.createElement("button");//delete button
-
-	label.innerText=taskString;
-
-	checkBox.type="checkbox";
-	editInput.type="text";
-
-	editButton.innerText="Edit";
-	editButton.className="edit";
-	deleteButton.innerText="Delete";
-	deleteButton.className="delete";
-
-
-	listItem.appendChild(checkBox);
-	listItem.appendChild(label);
-	listItem.appendChild(editInput);
-	listItem.appendChild(editButton);
-	listItem.appendChild(deleteButton);
-	return listItem;
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-
-
-var addTask=function(){
-	console.log("Add Task...");
-	var listItem=createNewTaskElement(taskInput.value);
-
-	incompleteTaskHolder.appendChild(listItem);
-	bindTaskEvents(listItem, taskCompleted);
-
-	taskInput.value="";
-
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
-var editTask=function(){
-console.log("Edit Task...");
-console.log("Change 'edit' to 'save'");
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
-var listItem=this.parentNode;
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-var editInput=listItem.querySelector('input[type=text]');
-var label=listItem.querySelector("label");
-var containsClass=listItem.classList.contains("editMode");
-		if(containsClass){
-
-			label.innerText=editInput.value;
-		}else{
-			editInput.value=label.innerText;
-		}
-
-		listItem.classList.toggle("editMode");
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
-
-
-
-
-var deleteTask=function(){
-		console.log("Delete Task...");
-
-		var listItem=this.parentNode;
-		var ul=listItem.parentNode;
-		ul.removeChild(listItem);
-
-}
-
-
-var taskCompleted=function(){
-		console.log("Complete Task...");
-
-	var listItem=this.parentNode;
-	completedTasksHolder.appendChild(listItem);
-				bindTaskEvents(listItem, taskIncomplete);
-
-}
-
-
-var taskIncomplete=function(){
-		console.log("Incomplete Task...");
-		var listItem=this.parentNode;
-	incompleteTaskHolder.appendChild(listItem);
-			bindTaskEvents(listItem,taskCompleted);
-}
-
-
-
-var ajaxRequest=function(){
-	console.log("AJAX Request");
-}
-
-
-addButton.onclick=addTask;
-addButton.addEventListener("click",addTask);
-addButton.addEventListener("click",ajaxRequest);
-
-
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
-	console.log("bind list item events");
-	var checkBox=taskListItem.querySelector("input[type=checkbox]");
-	var editButton=taskListItem.querySelector("button.edit");
-	var deleteButton=taskListItem.querySelector("button.delete");
-
-
-			editButton.onclick=editTask;
-			deleteButton.onclick=deleteTask;
-			checkBox.onchange=checkBoxEventHandler;
-}
-
-	for (var i=0; i<incompleteTaskHolder.children.length;i++){
-
-		bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
-	}
-
-
-	for (var i=0; i<completedTasksHolder.children.length;i++){
-		bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
-	}
