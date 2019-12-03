48c5cc70e1b6c5bafcf8f484dbbf089582e2255a
guicgs <etohlif@protonmail.com>
2019-12-03 02:16:39 -0300

    concluindo uma parte do layout, ainda com problema no botão de descer os itens da lista

--- guicgs/listaTarefas.js
+++ guicgs/listaTarefas.js
@@ -4,6 +4,7 @@ texto.value = null;
 
 botaoAdicionar.addEventListener('click', cliqueAdicionar)
 
+//botão para adicionar elemento à lista
 function cliqueAdicionar() {
   if (texto.value == "");
   else {
@@ -18,15 +19,17 @@ function cliqueAdicionar() {
   }
 }
 
+// adicionar elemento à lista pressionando 'enter'
 texto.addEventListener('keypress', function (e) {
   if(e.keyCode == 13) {
     cliqueAdicionar();
   }
 })
 
+// destacar item quando selecionado
 let itemSelecionado = document.getElementsByClassName("item");
 function selecionarItem() {
-  if(this.style.backgroundColor == "rgb(189, 189, 189)") {
+  if(this.style.backgroundColor == "rgb(0, 145, 212)") {
     for(let element of itemSelecionado) {
     element.style.backgroundColor = "transparent";
     }
@@ -35,59 +38,67 @@ function selecionarItem() {
     for(let element2 of itemSelecionado) {
       element2.style.backgroundColor = "transparent";
       }
-  this.style.backgroundColor = "rgb(189, 189, 189)";
+  this.style.backgroundColor = "rgb(0, 145, 212)";
   }
 }
 
+// riscar item quando der dois cliques
 function riscarItem() {
   if(this.style.textDecoration == "line-through") {
     event.target.style.textDecoration = "";
+    this.style.color = "black";
   }
   else {
+    this.classList.add("riscado");
     this.style.textDecoration = "line-through";
+    this.style.color = "gray";
   }
 }
 
+//botão para subir item na lista
+let botaoSubirItem = document.getElementById("setaCima");
+botaoSubirItem.addEventListener('click', subirItem);
+function subirItem() {
+  for(let element3 of itemSelecionado) {
+    if(element3.style.backgroundColor == "rgb(0, 145, 212)") {
+        let pai = element3.parentNode;
+      if (element3 == pai.firstChild) {}
+      else {
+        let prev = element3.previousSibling;
+        let oldChild = pai.removeChild(element3);
+        pai.insertBefore(oldChild, prev);
+      }
+    }
+  }
+}
+
+// botão para descer itens na lista
 let botaoDescerItem = document.getElementById("setaBaixo");
 botaoDescerItem.addEventListener('click', descerItem);
 function descerItem() {
   for(let element3 of itemSelecionado) {
-    if(element3.style.backgroundColor == "rgb(189, 189, 189)") {
+    if(element3.style.backgroundColor == "rgb(0, 145, 212)") {
         let pai = element3.parentNode;
         if (element3 == pai.lastChild) {}
-    else {
+        else {
         pai.insertBefore(element3.nextElementSibling, element3);
       }
     }
   }
 }
 
-let botaoSubirItem = document.getElementById("setaCima");
-botaoSubirItem.addEventListener('click', subirItem);
-function subirItem() {
-  for(let element3 of itemSelecionado) {
-    if(element3.style.backgroundColor == "rgb(189, 189, 189)") {
-      let pai = element3.parentNode;
-      if (element3 == pai.firstChild) {}
-      else {
-      let prev = element3.previousSibling;
-      let oldChild = pai.removeChild(element3);
-      pai.insertBefore(oldChild, prev);
-      }
-    }
-  }
-}
-
+//botão para remover item selecionado
 let botaoRemoverItem = document.getElementById("removerItem");
 botaoRemoverItem.addEventListener('click', removerItem);
 function removerItem() {
   for(let element4 of itemSelecionado) {
-    if(element4.style.backgroundColor == "rgb(189, 189, 189)") {
+    if(element4.style.backgroundColor == "rgb(0, 145, 212)") {
         element4.remove();
     }
   }
 }
 
+// botão para eliminar tarefas concluídas
 let botaoEliminarRiscados = document.getElementById("limparCumpridas");
 botaoEliminarRiscados.addEventListener('click', eliminarItensRiscados);
 function eliminarItensRiscados() {
@@ -99,6 +110,7 @@ function eliminarItensRiscados() {
   }
 }
 
+// botão para eliminar toda a lista
 let botaoEliminarTudo = document.getElementById("eliminarTudo");
 botaoEliminarTudo.addEventListener('click', eliminarTudo);
 function eliminarTudo() {
