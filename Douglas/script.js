f7e2367eaae9a82be10f70ee2b400a25c8ebc821
Douglas Henrique <doughrqe@gmail.com>
2019-12-06 14:00:01 -0300

    Possiveis Correções

--- Douglas/script.js
+++ Douglas/script.js
@@ -1,148 +1,139 @@
-const ok = document.getElementById('envia');
-const entrada = document.getElementById('entrada');
-const pegaOl = document.querySelector('ol');
-const botoes = document.querySelectorAll('button');
-const apagaTudo = document.getElementById('apagarTudo');
-const apagaRealizado = document.getElementById('apagarCompletos');
-let apaga = document.querySelectorAll('.risca');
-
-
-function removeTudo() {
-  apagaTudo.addEventListener('click', function () {
-    const pegandoOl = document.querySelector('ol');
-    pegandoOl.innerHTML = '';
-  });
+window.onload = function() {
+  let botao = document.getElementById('diretorio');
+  let texto = document.getElementById("entrada");
+  let escreve = document.getElementById("texto");
+  let texto2 = document.getElementById("entrada2")
+  let escreve2 = document.getElementById("texto1");
+  let up = document.getElementById('up');
+  let down = document.getElementById('down');
+  escreve.style.fontSize = '40px';
+  escreve2.style.fontSize = '40px';
+  function criaImagem() {
+    let image = document.getElementById('pegarTag');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image.alt = "gerador de memes";
+    image.style.width = "900px";
+    image.style.height = "500px";
 }
 
-
-function salvar() {
-  const botaoSalvar = document.getElementById('salvar');
-  botaoSalvar.addEventListener('click', function () {
-    localStorage.clear();
-    let li = document.querySelectorAll('.item');
-
-    for (let i = 0; i < li.length; i+=1) {
-      localStorage.setItem(i, li[i].innerHTML);
-    }
-  });
-}
-function recupera(criaLi) {
-  let li = localStorage.length;
-  for (let j = 0 ; j < li ; j+=1) {
-    const criarLi = document.createElement('li');
-    let novaString = localStorage.getItem(j);
-    criarLi.innerHTML = novaString;
-    criarLi.className = 'item';
-    pegaOl.appendChild(criarLi);
-  }
-}
-function risca(criaLi) {
-  criaLi.addEventListener('dblclick', function () {
-    criaLi.classList.toggle('risca');
-    apaga = document.querySelectorAll('.risca');
-  });
-}
-
-function marca(criaLi) {
-  criaLi.addEventListener('click', function () {
-    const destacado = document.querySelectorAll('.destaca');
-    if (destacado[0] === event.target) {
-      event.target.classList.toggle('destaca');
-    } else if (destacado[0] !== undefined) {
-      destacado[0].className = '';
-      event.target.classList.toggle('destaca');
-    } else {
-      criaLi.classList.toggle('destaca');
-    }
-  });
-}
-
-function moveCima() {
-  const botaoCima = document.getElementById('moveCima');
-  botaoCima.addEventListener('click', function () {
-    let movido = document.querySelector('.destaca');
-    const itens = movido.previousSibling;
-    if (itens.innerHTML === undefined) {
-      alert('A primeira posição não sobe, ela ja esta em primeiro !!!');
-    } else {
-      let auxiliar = itens.innerHTML;
-      itens.innerHTML = movido.innerHTML;
-      movido.innerHTML = auxiliar;
-    }
-  });
-}
-
-function moveBaixo (){
-  const botaoBaixo = document.getElementById('moveBaixo');
-  botaoBaixo.addEventListener('mouseup', function () {
-    let movido = document.querySelector('.destaca');
-    const itens = movido.nextSibling;
-    if (itens === null) {
-      alert('A ultima posição não Desce!')
-    } else {
-      let auxiliar = movido.innerHTML;
-      movido.innerHTML = itens.innerHTML;
-      itens.innerHTML = auxiliar;
-    }
-  });
-}
-
-function removeSelecao (){
-  const remover = document.getElementById('remove');
-  remover.addEventListener('click', function() {
-    let movido = document.querySelector('.destaca');
-    pegaOl.removeChild(movido);
-  });
-}
-
-function adicionaLinha(argumento) {
-  if (argumento === '') {
-    alert('Você deve adicionar alguma informação!');
+botao.addEventListener('change', function() {
+    criaImagem();
+});
+texto.addEventListener('input', function() {
+  if (texto.value.length > 100) {
+      alert('É um meme e não a biblia');
   } else {
-    const criaLi = document.createElement('li');
-    pegaOl.appendChild(criaLi);
-    criaLi.innerText = argumento;
-    criaLi.classList = ('class', 'item');
-    entrada.value = '';
-    risca(criaLi);
-    marca(criaLi);
+      escreve.innerHTML = texto.value;
   }
-}
+})
 
-function click() {
-  ok.addEventListener('click', function () {
-    adicionaLinha(entrada.value);
-  });
-}
-
-function trocaMouse() {
-  for (let i = 0; i < botoes.length; i += 1) {
-    botoes[i].addEventListener('mouseover', function () {
-      botoes[i].style.cursor = 'pointer';
-      botoes[i].style.backgroundColor = 'cornsilk';
-    });
-    botoes[i].addEventListener('mouseout', function () {
-      botoes[i].style.backgroundColor = 'ButtonFace';
-    });
+texto2.addEventListener('input', function() {
+  if (texto2.value.length >= 90) {
+      alert('É um meme e não a biblia');
+  } else {
+      escreve2.innerHTML = texto2.value;
   }
-}
+})
+up.addEventListener('click', function() {
 
-function apagaRiscado() {
-  apagaRealizado.addEventListener('click', function () {
-    for (let i = 0; i < apaga.length; i += 1) {
-      pegaOl.removeChild(apaga[i]);
+  if (parseInt(escreve.style.fontSize) >= 70) {
+      alert('Pra que isso tudo de fonte');
+    } else {
+      escreve.style.fontSize = (parseInt(escreve.style.fontSize) + 10 + 'px');
+      escreve2.style.fontSize = (parseInt(escreve2.style.fontSize) + 10 + 'px');
+  }
+});
+down.addEventListener('click', function() {
+  if (parseInt(escreve.style.fontSize) <= 20) {
+      alert('Ai não vai dar nem pra ler');
+    } else {
+      escreve.style.fontSize = (parseInt(escreve.style.fontSize) - 10 + 'px');
+      escreve2.style.fontSize = (parseInt(escreve2.style.fontSize) - 10 + 'px');
+  }
+})
+let mem1 = document.getElementById('mem1');
+mem1.addEventListener('click', function() {
+  let x = 'meme1'
+  sugestion(mem1, x)
+  image.alt = 'Garoto Rico Branco'
+})
+let mem2 = document.getElementById('mem2');
+mem2.addEventListener('click', function() {
+  let x = 'meme2'
+  sugestion(mem2, x)
+})
+let mem3 = document.getElementById('mem3');
+mem3.addEventListener('click', function() {
+  let x = 'meme3'
+  sugestion(mem3, x)
+
+})
+
+function sugestion(mem1, x) {
+  let image = document.getElementById('pegarTag');
+  let meme = document.getElementById(x);
+  image.src = meme.src;
+  image.alt = "gerador de memes";
+  image.style.width = "900px";
+  image.style.height = "500px";
+}
+let arrayBordas = document.getElementsByClassName('local');
+arrayBordas[0].addEventListener('click', function() {
+  let box = document.getElementsByClassName('box');
+  box[0].style.border = "double blue 5px";
+})
+arrayBordas[1].addEventListener('click', function() {
+  let box = document.getElementsByClassName('box');
+  box[0].style.border = 'blueviolet dashed 5px';
+})
+arrayBordas[2].addEventListener('click', function() {
+  let box = document.getElementsByClassName('box');
+  box[0].style.border = 'inset chocolate 5px';
+})
+let bordaPadrao = document.getElementsByClassName('border4');
+bordaPadrao[0].addEventListener('click', function() {
+  let box = document.getElementsByClassName('box');
+  box[0].style.border = 'solid 5px black';
+})
+let cor = document.getElementById('cores');
+
+cor.addEventListener('click', function() {
+  switch (cor.selectedIndex) {
+    case 1:
+                escreve.style.color = 'white';
+                escreve2.style.color = 'white';
+                voltaSombra();
+                break;
+            case 2:
+                escreve.style.color = 'black';
+                escreve2.style.color = 'black';
+                escreve.style.textShadow = '3px 3px white';
+                escreve2.style.textShadow = '3px 3px white';
+                break;
+            case 3:
+                escreve.style.color = 'yellow';
+                escreve2.style.color = 'yellow';
+                voltaSombra();
+                break;
+            case 4:
+                escreve.style.color = 'red';
+                escreve2.style.color = 'red';
+                voltaSombra();
+                break;
+            case 5:
+                escreve.style.color = 'green';
+                escreve2.style.color = 'green';
+                voltaSombra();
+                break;
+            case 6:
+                escreve.style.color = 'blue';
+                escreve2.style.color = 'blue';
+                voltaSombra();
+                break;
+        }
+    })
+
+    function voltaSombra() {
+        escreve.style.textShadow = '3px 3px black';
+        escreve2.style.textShadow = '3px 3px black';
     }
-  });
-}
-
-window.onload = function inicia() {
-  click();
-  trocaMouse();
-  removeTudo();
-  apagaRiscado();
-  moveCima();
-  moveBaixo();
-  removeSelecao();
-  salvar();
-  recupera();
-};
